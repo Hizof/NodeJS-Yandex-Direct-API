cd8aef92fe8fb52a0bc1ea53670c369ecb3eafc3
Hizof <ddbibin@yandex.ru>
2016-05-26 23:04:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+﻿var direct = require('./direct');
+
+direct.setToken("TOKEN");
+
+var apiV4 = direct.getV4();
+
+apiV4.campaign.getCampaignsList(null, function(data, err){console.log(data);});
+apiV4.keywords.getKeywordsSuggestion({Keywords: ['холодильник','камера']}, function(data, err){console.log(data);});
+
+var archiveCampaignParam = {
+	CampaignID: 999999
+};
+
+apiV4.campaign.archiveCampaign(archiveCampaignParam, function(data, err){});
