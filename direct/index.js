794f3706bf15695b9d6379ff580c8c4bf6c393c1
Hizof <ddbibin@yandex.ru>
2016-05-26 23:00:45 +0300

    Add files via upload

---
+++ direct/index.js
@@ -0,0 +1,29 @@
+﻿//var _token = "";
+var _isSendBox = false;
+var helper = require('./v4/helper');
+
+module.exports = {
+  setToken: function(token)
+  {
+	//_token = token;
+	helper.setToken(token);
+  },
+  setSandBox: function(isSendBox)
+  {
+	_isSendBox = isSendBox;
+  },
+  getV4: function()
+  {
+	return require('./v4');
+  },
+  getV4Live: function()
+  {
+	throw new Error("NotImplementedException");
+	return require('./v4Live');
+  },
+  getV5: function()
+  {
+	throw new Error("NotImplementedException");
+	return require('./v5');
+  }
+};
