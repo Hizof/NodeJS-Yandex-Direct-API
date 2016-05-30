a89dd31ece22399b40358df09f5cf69ece9bb92c
Hizof <ddbibin@yandex.ru>
2016-05-31 01:14:51 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-﻿var direct = require('./direct');
+var direct = require('./direct');
 
 direct.setToken("TOKEN");
+direct.setSandBox(true);
 
 var apiV4 = direct.getV4();
 
 apiV4.campaign.getCampaignsList(null, function(data, err){console.log(data);});
+
 apiV4.keywords.getKeywordsSuggestion({Keywords: ['холодильник','камера']}, function(data, err){console.log(data);});
+
 
 var archiveCampaignParam = {
 	CampaignID: 999999
